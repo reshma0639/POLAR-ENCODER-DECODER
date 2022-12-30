79e2c431e251154b0cee10956d362e437bc0853a
Mannava Venkatasai <111038679+Mannava123455@users.noreply.github.com>
2022-12-30 19:49:08 +0530

    Hamdan logic

--- FWC_2_polarencoder/codes/polar_encoder_verilog/logic.py
+++ FWC_2_polarencoder/codes/polar_encoder_verilog/logic.py
@@ -8,8 +8,8 @@ for i in range(0, level):
     src_cyc = 2**i
     dest_cyc = 2**(i+1)
     for j in range(0, N):
-        xor = f"inter_{i+1}[{j}] = inter_{i}[{j}]^inter_{i}[{j+src_cyc}];"
-        same = f"inter_{i+1}[{j}] = inter_{i}[{j}];"
+        xor = f"assign inter_{i+1}[{j}] = inter_{i}[{j}]^inter_{i}[{j+src_cyc}];"
+        same = f"assign inter_{i+1}[{j}] = inter_{i}[{j}];"
         if((j//src_cyc)%2==0):
             print(xor)
         else:
