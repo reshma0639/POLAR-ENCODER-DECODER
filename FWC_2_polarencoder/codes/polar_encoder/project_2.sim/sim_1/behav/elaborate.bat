29f6550e2b6107a851887dbbb0c009354d3dd005
Mannava Venkatasai <111038679+Mannava123455@users.noreply.github.com>
2022-12-30 19:24:57 +0530

    Delete FWC_2_polarencoder/codes directory

--- FWC_2_polarencoder/codes/polar_encoder/project_2.sim/sim_1/behav/elaborate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2017.2\\bin
-call %xv_path%/xelab  -wto 88494bdada5e4b0c8a44b841310a1cd7 -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L unisims_ver -L unimacro_ver -L secureip --snapshot polar_tb1_behav xil_defaultlib.polar_tb1 xil_defaultlib.glbl -log elaborate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
